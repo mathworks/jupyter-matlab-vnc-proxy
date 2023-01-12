757d102dad79b928224f76f2ae3e841df37a7202
Prabhakar Kumar <prabhakk@mathworks.com>
2023-01-12 13:59:24 +0530

    Update to v0.1.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 setuptools.setup(
     name="jupyter-matlab-vnc-proxy",
     packages=setuptools.find_packages(where='.'),
-    version='0.1.0',
+    version='0.1.2',
     description="Jupyter extension to provide MATLAB via VNC connection",
     keywords=["Jupyter"],
     entry_points={
